d35ad61b2f616eb0c60c062820e0c94010b9429c
Aman Agrawala <amanagrawala@gmail.com>
2015-12-23 17:42:57 -0600

    Comment out score and old matches table. Add correct matches table.

--- tournament.sql
+++ tournament.sql
@@ -10,6 +10,10 @@
 
 create table players (ID serial primary key, Name text);
 
-create table score (ID serial references players(id), Player text, Opponent text, Win/Loss text)
+--create table score (ID serial references players(id), Player text, Opponent text, Win/Loss text)
 
-create table matches (Round int, Match_Number int, Player1_ID serial references players(ID), Player1_Name text references players(Name), Player2_ID serial references players(ID), Players2_Name text references players(Name), Winner text, primary key (Round, Match_Number));
+--create table matches (Round int, Match_Number int, Player1_ID serial references players(ID), Player1_Name text references players(Name), Player2_ID serial references players(ID), Players2_Name text references players(Name), Winner text, primary key (Round, Match_Number));
+
+-- create table matches (Round int, Match_Number int, Player1_ID serial references players(ID), Player2_ID serial references players(ID), Winner serial references players(ID), primary key (Round, Match_Number));
+
+create table matches (Match_Number serial primary key, Winner serial references players(ID), Loser serial references players(ID));
