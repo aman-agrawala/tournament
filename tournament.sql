456701b550ad221357008c7fc7720bea9b28b971
Aman Agrawala <amanagrawala@gmail.com>
2015-12-23 17:44:04 -0600

    Add match_number table to view total matches.

--- tournament.sql
+++ tournament.sql
@@ -17,3 +17,5 @@ create table players (ID serial primary key, Name text);
 -- create table matches (Round int, Match_Number int, Player1_ID serial references players(ID), Player2_ID serial references players(ID), Winner serial references players(ID), primary key (Round, Match_Number));
 
 create table matches (Match_Number serial primary key, Winner serial references players(ID), Loser serial references players(ID));
+
+create view match_number as (select players.ID, players.Name, count(matches.Winner) as matchs from players left join matches on players.ID = matches.Winner or players.ID = matches.Loser group by matches.Winner, matches.Loser, players.ID order by matchs);
