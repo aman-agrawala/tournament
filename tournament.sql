e0a3a7de485ec24701ce1feecdab0986b292e565
Aman Agrawala <amanagrawala@gmail.com>
2015-12-23 23:53:13 -0600

    Add standings table which lists players and their win records, sorted by wins.

--- tournament.sql
+++ tournament.sql
@@ -16,8 +16,10 @@ create table players (ID serial primary key, Name text);
 
 -- create table matches (Round int, Match_Number int, Player1_ID serial references players(ID), Player2_ID serial references players(ID), Winner serial references players(ID), primary key (Round, Match_Number));
 
-create table matches (Match_Number serial primary key, Winner serial references players(ID), Loser serial references players(ID));
+create table matches (Match_Number serial primary key, Winner int references players(ID), Loser int references players(ID));
 
 create view match_number as (select players.ID, players.Name, count(matches.Winner) as matchs from players left join matches on players.ID = matches.Winner or players.ID = matches.Loser group by matches.Winner, matches.Loser, players.ID order by matchs);
 
 create view score as (select players.ID, players.Name, count(matches.Winner) as wins, count(matches.Winner) as match_num from players left join matches on players.ID = matches.Winner group by matches.Winner, players.ID order by wins);
+
+create view standings as (select score.ID, score.Name, score.wins, match_number.matchs as matches from score join match_number on score.ID = match_number.ID order by wins DESC);
